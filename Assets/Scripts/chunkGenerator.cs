0fd06b5e287464fdc3ffdec097f60a05730a5d5c
davidluca03 <davidlucaionut123@gmail.com>
2025-08-12 12:48:40 +0300

    terrain parameters dynamic update at runtime and shader configurations

--- Assets/Scripts/chunkGenerator.cs
+++ Assets/Scripts/chunkGenerator.cs
@@ -30,15 +30,304 @@ public class chunkGenerator : MonoBehaviour
     public float noiseBias = 0.0f;
     public Boolean absoluteHeight = false;
     public Color fogColor = Color.white;
+    public float fogStart = 0.0f;
     public Material basicMaterial;
+    public Boolean UseBasicMaterial = true;
+    public Color planeColor = Color.darkGreen;
+    public Color slopeColor = Color.dimGray;
+    public float slopeBias = 0.5f;
+    public float slopeSharpness = 25.0f;
+    private Boolean previousUseBasicMaterial = true;
+    private Color previousPlaneColor = Color.darkGreen;
+    private Color previousSlopeColor = Color.dimGray;
+    private float previousSlopeBias = 0.5f;
+    private float previousSlopeSharpness = 25.0f;
     private HashSet<Vector2> generatedChunks = new HashSet<Vector2>();
     private Dictionary<Vector2, GameObject> chunkObjects = new Dictionary<Vector2, GameObject>();
     private Vector3 refferencePosition;
-
+    private Color previousFogColor = Color.white;
+    private float previousFogStart = 0.0f;
+    private float previousOctaves = 1.0f;
+    private float previousAmplitude = 1.0f;
+    private float previousPerlinScale = 1.0f;
+    private float previousPersistence = 0.5f;
+    private float previousFrequency = 1.0f;
+    private float previousLacunarity = 2.0f;
+    private float previousUVScale = 1.0f;
+    private float previousSharpness = 1.0f;
+    private float previousMaxHeight = 1.0f;
+    private float previousNoiseBias = 0.0f;
+    private Boolean previousAbsoluteHeight = false;
+    private float updateInterval = 1.0f;
+    private float timer = 0.0f;
     void Start()
     {
+        previousAmplitude = amplitude;
+        previousPerlinScale = perlinScale;
+        previousPersistence = persistence;
+        previousFrequency = frequency;
+        previousLacunarity = lacunarity;
+        previousUVScale = UVscale;
+        previousFogColor = fogColor;
+        previousSharpness = sharpness;
+        previousMaxHeight = maxHeight;
+        previousNoiseBias = noiseBias;
+        previousOctaves = octaves;
+        previousUseBasicMaterial = UseBasicMaterial;
+        previousPlaneColor = planeColor;
+        previousSlopeColor = slopeColor;
+        previousSlopeBias = slopeBias;
+        previousSlopeSharpness = slopeSharpness;
+        previousAbsoluteHeight = absoluteHeight;
+        previousFogStart = fogStart;
     }
 
+    private Boolean equalColors(Color color1, Color color2, float tolerance = 0.01f) {
+        return Mathf.Abs(color1.r - color2.r) < tolerance &&
+               Mathf.Abs(color1.g - color2.g) < tolerance &&
+               Mathf.Abs(color1.b - color2.b) < tolerance &&
+               Mathf.Abs(color1.a - color2.a) < tolerance;
+    }
+
+    private Boolean compareFloats(float a, float b, float tolerance = 0.01f) {
+        return Mathf.Abs(a - b) < tolerance;
+    }
+
+    private void UpdateAllChunks(Action<chunkScript> updateAction)
+    {
+        foreach (var chunk in chunkObjects.Values)
+        {
+            if (chunk.TryGetComponent(out chunkScript chunkComponent))
+            {
+                updateAction(chunkComponent);
+            }
+        }
+    }
+
+    private void updateOctaves(float newOctaves)
+    {
+        UpdateAllChunks(chunk => chunk.updateOctaves(newOctaves));
+    }
+
+    private void updateAmplitude(float newAmplitude)
+    {
+        UpdateAllChunks(chunk => chunk.updateAmplitude(newAmplitude));
+    }
+
+    private void updatePersistence(float newPersistence)
+    {
+        UpdateAllChunks(chunk => chunk.updatePersistence(newPersistence));
+    }
+
+    private void updateFrequency(float newFrequency)
+    {
+        UpdateAllChunks(chunk => chunk.updateFrequency(newFrequency));
+    }
+
+    private void updateLacunarity(float newLacunarity)
+    {
+        UpdateAllChunks(chunk => chunk.updateLacunarity(newLacunarity));
+    }
+
+    private void updatePerlinScale(float newPerlinScale)
+    {
+        UpdateAllChunks(chunk => chunk.updatePerlinScale(newPerlinScale));
+    }
+
+    private void updateFogColor(Color newFogColor)
+    {
+        UpdateAllChunks(chunk => chunk.updateFogColor(newFogColor));
+    }
+
+    private void updateFogStart(float newFogStart)
+    {
+        UpdateAllChunks(chunk => chunk.updateFogStart(newFogStart));
+    }
+
+    private void updateUseBasicMaterial(bool newUseBasicMaterial)
+    {
+        UpdateAllChunks(chunk => chunk.updateUsingBasicMaterial(newUseBasicMaterial));
+    }
+
+    private void updatePlaneColor(Color newPlaneColor)
+    {
+        UpdateAllChunks(chunk => chunk.updatePlaneColor(newPlaneColor));
+    }
+
+    private void updateSlopeColor(Color newSlopeColor)
+    {
+        UpdateAllChunks(chunk => chunk.updateSlopeColor(newSlopeColor));
+    }
+
+    private void updateSlopeBias(float newSlopeBias)
+    {
+        UpdateAllChunks(chunk => chunk.updateSlopeBias(newSlopeBias));
+    }
+
+    private void updateSlopeSharpness(float newSlopeSharpness)
+    {
+        UpdateAllChunks(chunk => chunk.updateSlopeSharpness(newSlopeSharpness));
+    }
+
+    private void updateUVScale(float newUVScale)
+    {
+        UpdateAllChunks(chunk => chunk.updateUVScale(newUVScale));
+    }
+
+    private void updateSharpness(float newSharpness)
+    {
+        UpdateAllChunks(chunk => chunk.updateSharpness(newSharpness));
+    }
+
+    private void updateMaxHeight(float newMaxHeight)
+    {
+        UpdateAllChunks(chunk => chunk.updateMaxHeight(newMaxHeight));
+    }
+
+    private void updateNoiseBias(float newNoiseBias)
+    {
+        UpdateAllChunks(chunk => chunk.updateNoiseBias(newNoiseBias));
+    }
+
+    private void updateAbsoluteHeight(Boolean newAbsoluteHeight)
+    {
+        UpdateAllChunks(chunk => chunk.updateAbsoluteHeight(newAbsoluteHeight));
+    }
+
+    private void UpdateAttributesCheck()
+    {
+        if (!compareFloats(previousAmplitude, amplitude))
+        {
+            updateAmplitude(amplitude);
+            previousAmplitude = amplitude;
+            Debug.Log("Amplitude updated to: " + amplitude);
+        }
+
+        if (!compareFloats(previousPersistence, persistence))
+        {
+            updatePersistence(persistence);
+            previousPersistence = persistence;
+            Debug.Log("Persistence updated to: " + persistence);
+        }
+
+        if (!compareFloats(previousFrequency, frequency))
+        {
+            updateFrequency(frequency);
+            previousFrequency = frequency;
+            Debug.Log("Frequency updated to: " + frequency);
+        }
+
+        if (!compareFloats(previousLacunarity, lacunarity))
+        {
+            updateLacunarity(lacunarity);
+            previousLacunarity = lacunarity;
+            Debug.Log("Lacunarity updated to: " + lacunarity);
+        }
+
+        if (!compareFloats(previousPerlinScale, perlinScale))
+        {
+            updatePerlinScale(perlinScale);
+            previousPerlinScale = perlinScale;
+            Debug.Log("Perlin scale updated to: " + perlinScale);
+        }
+
+        if (!compareFloats(previousUVScale, UVscale))
+        {
+            updateUVScale(UVscale);
+            previousUVScale = UVscale;
+            Debug.Log("UV scale updated to: " + UVscale);
+        }
+
+        if (!equalColors(previousFogColor, fogColor))
+        {
+            updateFogColor(fogColor);
+            previousFogColor = fogColor;
+        }
+
+        if (!compareFloats(previousFogStart, fogStart))
+        {
+            updateFogStart(fogStart);
+            previousFogStart = fogStart;
+            Debug.Log("Fog start updated to: " + fogStart);
+        }
+
+        if (previousUseBasicMaterial != UseBasicMaterial)
+        {
+            updateUseBasicMaterial(UseBasicMaterial);
+            previousUseBasicMaterial = UseBasicMaterial;
+        }
+
+        if (!equalColors(previousPlaneColor, planeColor))
+        {
+            updatePlaneColor(planeColor);
+            previousPlaneColor = planeColor;
+        }
+
+        if (!equalColors(previousSlopeColor, slopeColor))
+        {
+            updateSlopeColor(slopeColor);
+            previousSlopeColor = slopeColor;
+        }
+
+        if (!compareFloats(previousSlopeBias, slopeBias))
+        {
+            updateSlopeBias(slopeBias);
+            previousSlopeBias = slopeBias;
+            Debug.Log("Slope bias updated to: " + slopeBias);
+        }
+
+        if (!compareFloats(previousSlopeSharpness, slopeSharpness))
+        {
+            updateSlopeSharpness(slopeSharpness);
+            previousSlopeSharpness = slopeSharpness;
+            Debug.Log("Slope sharpness updated to: " + slopeSharpness);
+        }
+
+        if (!compareFloats(previousSharpness, sharpness))
+        {
+            updateSharpness(sharpness);
+            previousSharpness = sharpness;
+            Debug.Log("Sharpness updated to: " + sharpness);
+        }
+
+        if (!compareFloats(previousMaxHeight, maxHeight))
+        {
+            updateMaxHeight(maxHeight);
+            previousMaxHeight = maxHeight;
+            Debug.Log("Max height updated to: " + maxHeight);
+        }
+
+        if (!compareFloats(previousNoiseBias, noiseBias))
+        {
+            updateNoiseBias(noiseBias);
+            previousNoiseBias = noiseBias;
+            Debug.Log("Noise bias updated to: " + noiseBias);
+        }
+
+        if (!compareFloats(previousOctaves, octaves))
+        {
+            updateOctaves(octaves);
+            previousOctaves = octaves;
+            Debug.Log("Octaves updated to: " + octaves);
+        }
+
+        if (previousAbsoluteHeight != absoluteHeight)
+        {
+            updateAbsoluteHeight(absoluteHeight);
+            previousAbsoluteHeight = absoluteHeight;
+            Debug.Log("Absolute height updated to: " + absoluteHeight);
+        }
+    }
+
+    private void checkForUpdates()
+    {
+        timer += Time.deltaTime;
+        if (timer >= updateInterval)
+        {
+            timer = 0.0f;
+            UpdateAttributesCheck();
+        }
+    }
 
     private void despawnChunks()
     {
@@ -98,11 +387,23 @@ public class chunkGenerator : MonoBehaviour
                         chunk.offsetX = offsetX + i * chunkSize * chunkScale;
                         chunk.offsetZ = offsetZ + j * chunkSize * chunkScale;
                         chunk.amplitude = amplitude;
-                        chunk.material = basicMaterial;
+                        chunk.basicMaterial = basicMaterial;
+                        chunk.UseBasicMaterial = UseBasicMaterial;
+                        chunk.planeColor = planeColor;
+                        chunk.slopeColor = slopeColor;
+                        chunk.slopeBias = slopeBias;
+                        chunk.slopeSharpness = slopeSharpness;
+                        chunk.fogColor = fogColor;
+                        chunk.fogStart = fogStart;
+                        chunk.sharpness = sharpness;
+                        chunk.maxHeight = maxHeight;
+                        chunk.noiseBias = noiseBias;
+                        chunk.absoluteHeight = absoluteHeight;
                     }
                 }
             }
-        };
+        }
+        checkForUpdates();
         despawnChunks();
     }
 }
--- Assets/Scripts/chunkScript.cs
+++ Assets/Scripts/chunkScript.cs
@@ -19,15 +19,183 @@ public class chunkScript : MonoBehaviour
     public float offsetX = 0.0f;
     public float offsetZ = 0.0f;
     public float amplitude = 1.0f;
-    public Material material;
+    public float sharpness = 1.0f;
+    public float maxHeight = 1.0f;
+    public float noiseBias = 0.0f;
+    public Boolean absoluteHeight = false;
+    public Material basicMaterial;
+    public Boolean UseBasicMaterial = true;
+    public Color planeColor = Color.darkGreen;
+    public Color slopeColor = Color.dimGray;
+    public float slopeBias = 0.5f;
+    public float slopeSharpness = 25.0f;
+    public Color fogColor = Color.white;
+    public float fogStart = 0.0f;
     private Renderer objectRenderer;
+
+
+    private void OnDestroy()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter != null && meshFilter.mesh != null)
+        {
+            Destroy(meshFilter.mesh);
+            meshFilter.mesh = null;
+        }
+    }
+
+    public void updateMeshVertices()
+    {
+        MeshFilter meshFilter = GetComponent<MeshFilter>();
+        if (meshFilter != null && meshFilter.mesh != null)
+        {
+            meshFilter.mesh.Clear();
+
+            Vector3[] vertices = createVertices(chunkSize, chunkScale);
+            int[] triangles = createTriangles(chunkSize);
+            Vector2[] uvs = createUVs(chunkSize);
+
+            meshFilter.mesh.vertices = vertices;
+            meshFilter.mesh.triangles = triangles;
+            meshFilter.mesh.uv = uvs;
+
+            meshFilter.mesh.RecalculateNormals();
+            meshFilter.mesh.RecalculateBounds();
+        }
+    }
+
+    public void updateOctaves(float newOctaves)
+    {
+        octaves = newOctaves;
+        updateMeshVertices();
+    }
+
+    public void updateAmplitude(float newAmplitude)
+    {
+        amplitude = newAmplitude;
+        updateMeshVertices();
+    }
+
+    public void updatePersistence(float newPersistence)
+    {
+        persistence = newPersistence;
+        updateMeshVertices();
+    }
+
+    public void updateFrequency(float newFrequency)
+    {
+        frequency = newFrequency;
+        updateMeshVertices();
+    }
+
+    public void updateLacunarity(float newLacunarity)
+    {
+        lacunarity = newLacunarity;
+        updateMeshVertices();
+    }
+
+    public void updatePerlinScale(float newPerlinScale)
+    {
+        perlinScale = newPerlinScale;
+        updateMeshVertices();
+    }
+
+    public void updateUVScale(float newUVScale) 
+    {
+        UVscale = newUVScale;
+        updateMeshVertices();
+    }
+
+    public void updateSharpness(float newSharpness)
+    {
+        sharpness = newSharpness;
+        updateMeshVertices();
+    }
+
+    public void updateMaxHeight(float newMaxHeight)
+    {
+        maxHeight = newMaxHeight;
+        updateMeshVertices();
+    }
+
+    public void updateAbsoluteHeight(Boolean newAbsoluteHeight)
+    {
+        absoluteHeight = newAbsoluteHeight;
+        updateMeshVertices();
+    }
+
+    public void updateNoiseBias(float newNoiseBias)
+    {
+        noiseBias = newNoiseBias;
+        updateMeshVertices();
+    }
+    
+    public void updateFogColor(Color newFogColor)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetColor("_fogColor", newFogColor);
+    }
+
+    public void updateFogStart(float newFogStart)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetFloat("_fogStart", newFogStart);
+    }
+
+    public void updateUsingBasicMaterial(Boolean newUseBasicMaterial)
+    {
+        UseBasicMaterial = newUseBasicMaterial;
+        //TODO
+    }
+
+    public void updatePlaneColor(Color newPlaneColor)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetColor("_planeColor", newPlaneColor);
+    }
+
+    public void updateSlopeColor(Color newSlopeColor)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetColor("_slopeColor", newSlopeColor);
+    }
+
+    public void updateSlopeBias(float newSlopeBias)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetFloat("_slopeBias", newSlopeBias);
+    }
+
+    public void updateSlopeSharpness(float newSlopeSharpness)
+    {
+        if (objectRenderer == null)
+        {
+            objectRenderer = GetComponent<Renderer>();
+        }
+        objectRenderer.sharedMaterial.SetFloat("_slopeSharpness", newSlopeSharpness);
+    }
 
     public void Awake()
     {
-        if (material == null)
+        if (basicMaterial == null)
         {
-            material = new Material(Shader.Find("Standard"));
-            material.color = Color.white;
+            basicMaterial = new Material(Shader.Find("Standard"));
+            basicMaterial.color = Color.white;
         }
     }
 
@@ -38,14 +206,22 @@ public class chunkScript : MonoBehaviour
         float ampDecay = 1.0f;
         for (int i = 0; i < octaves; i++)
         {
-            height += (Mathf.PerlinNoise(x * freq, z * freq) * 2 - 1) * ampDecay;
+            height += Mathf.Min(Mathf.PerlinNoise(x * freq, z * freq) * 2 - 1 + noiseBias, maxHeight) * ampDecay;
             maxAmp += ampDecay;
             freq *= lacunarity;
             amp *= persistence;
             ampDecay *= persistence;
         }
         //return (Math.Abs(Math.Abs(height / maxAmp) - 0.8f) - 0.5f) * amplitude;
-        return (float) Math.Abs((Math.Pow(height / maxAmp, 2))) * amplitude;
+        if (absoluteHeight)
+        {
+            return (float)Math.Abs(Math.Pow(height / maxAmp, sharpness)) * amplitude;
+        }
+        else
+        {
+            return (float) Math.Pow(height / maxAmp, sharpness) * amplitude; 
+        }
+        
     }
 
     private Vector3[] createVertices(int size, float scale)
@@ -120,7 +296,7 @@ public class chunkScript : MonoBehaviour
             meshRenderer = gameObject.AddComponent<MeshRenderer>();
 
         Mesh mesh = new Mesh();
-        meshRenderer.material = material;
+        meshRenderer.sharedMaterial = basicMaterial;
 
         mesh.vertices = createVertices(chunkSize, chunkScale);
         mesh.triangles = createTriangles(chunkSize);
@@ -130,6 +306,16 @@ public class chunkScript : MonoBehaviour
         meshFilter.mesh = mesh;
 
         objectRenderer = GetComponent<Renderer>();
+        objectRenderer.sharedMaterial.SetColor("_fogColor", fogColor);
+
+        if (UseBasicMaterial)
+        {
+            objectRenderer.sharedMaterial = basicMaterial;
+            objectRenderer.sharedMaterial.SetColor("_planeColor", planeColor);
+            objectRenderer.sharedMaterial.SetColor("_slopeColor", slopeColor);
+            objectRenderer.sharedMaterial.SetFloat("_slopeBias", slopeBias);
+            objectRenderer.sharedMaterial.SetFloat("_slopeSharpness", slopeSharpness);
+        }
     }
     void Start()
     {
@@ -138,7 +324,7 @@ public class chunkScript : MonoBehaviour
 
     public void setAttributes(int size, float scale, float uvScale, float perlinScale, float octaves,
         float persistence, float lacunarity, float frequency, float offsetX, float offsetZ,
-        float amplitude)
+        float amplitude, Color fogColor, float sharpness)
     {
         this.chunkSize = size;
         this.chunkScale = scale;
@@ -151,6 +337,8 @@ public class chunkScript : MonoBehaviour
         this.offsetX = offsetX;
         this.offsetZ = offsetZ;
         this.amplitude = amplitude;
+        this.fogColor = fogColor;
+        this.sharpness = sharpness;
     }
 
     // Update is called once per frame
